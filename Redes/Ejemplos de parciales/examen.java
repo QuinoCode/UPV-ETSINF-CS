d48d2f677944205d07c3f93d74fe631f33cdd6d4
Quino <joaquinangelvalinteruiz@gmail.com>
2022-11-14 12:02:19 +0100

    Added a folder under Redes
    The folder contains an example of an exam, specifically the one I went through

---
+++ Redes/Ejemplos de parciales/examen.java
@@ -0,0 +1,48 @@
+import java.net.*;
+import java.util.*;
+
+import java.io.*;
+public class examen
+{
+	public static void main(String[] args)
+	{
+		try 
+		{
+			InetAddress upv = InetAddress.getByName("zoltar.redes.upv.es.");
+			Socket s = new Socket((upv), 80);//El 80 lo cambias por el puerto que te den
+			Scanner entrada = new Scanner(s.getInputStream());
+			Scanner teclado = new Scanner(System.in);
+			//PrintWriter salida = new PrintWriter(s.getOuptputStream(), false);
+			PrintWriter salida = new PrintWriter(s.getOutputStream(), false);
+			salida.println(teclado.nextInt());
+			salida.flush();
+			String respuesta = entrada.nextLine();
+			System.out.println(respuesta);
+			while (!respuesta.startsWith(".")) 
+			{
+				salida.println(teclado.nextInt());
+				salida.flush();
+				System.out.println(respuesta);
+				respuesta = entrada.nextLine();
+				
+			}
+			System.out.println("Acertado");
+			s.close();
+			System.out.println("Desconectado");
+			
+			
+
+		}
+		catch (NoRouteToHostException e) 
+		{
+			System.out.println("Host desconocido");
+			System.out.println(e);
+		}
+		catch (IOException e)
+		{
+			System.out.println("No se pudo conectar");
+			System.out.println(e);
+		}
+
+	}
+}
